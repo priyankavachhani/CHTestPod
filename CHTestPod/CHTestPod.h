8a455642c7e63ab84a6560e4507e0f8eddd8a797
priyankavachhani@callhippo.com <priyankavachhani@callhippo.com>
2021-01-27 11:24:24 +0530

    Initial project setup

---
+++ CHTestPod/CHTestPod.h
@@ -0,0 +1,18 @@
+//
+//  CHTestPod.h
+//  CHTestPod
+//
+//  Created by Apple on 27/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CHTestPod.
+FOUNDATION_EXPORT double CHTestPodVersionNumber;
+
+//! Project version string for CHTestPod.
+FOUNDATION_EXPORT const unsigned char CHTestPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHTestPod/PublicHeader.h>
+
+
